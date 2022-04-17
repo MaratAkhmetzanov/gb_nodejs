90b07eeca23b86845af0c5ad1a1d7937e1bd5d2e
why_mario <ahmetzanovmarat@gmail.com>
2022-04-17 15:17:52 +0300

    lesson 3

---
+++ gb_nodejs_lesson_3/index.js
@@ -0,0 +1,41 @@
+import fs, { createReadStream, createWriteStream } from 'fs';
+import readLine from 'readline';
+
+const ACESS_LOG = './gb_nodejs_lesson_3/access.log';
+const TEMP_LOG = './gb_nodejs_lesson_3/temp.log';
+
+const ipList = [
+  '89.123.1.41',
+  '34.48.240.111'
+]
+
+let writeStreams = [];
+
+const readStream = createReadStream(ACESS_LOG, {
+  encoding: 'utf-8',
+  highWaterMark: 64
+});
+
+ipList.forEach(element => {
+  const wStream = createWriteStream(`./gb_nodejs_lesson_3/${element}_requests.log`, {
+    encoding: 'utf-8',
+    flags: 'a'
+  });
+  writeStreams[element] = wStream;
+});
+
+const rl = readLine.createInterface({
+  input: readStream,
+  crlfDelay: Infinity
+});
+
+for await (const line of rl) {
+  ipList.forEach(element => {
+    if (line.includes(element)) {
+      writeStreams[element].write(line + '\n',
+        (err) => {
+          if (err) console.log(err)
+        })
+    }
+  });
+};
