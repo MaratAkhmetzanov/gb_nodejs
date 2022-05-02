dda3e3cc8d7fa33de1a9883af2b916c2693a2d17
why_mario <ahmetzanovmarat@gmail.com>
2022-05-02 16:56:08 +0300

    lesson 6

---
+++ lesson_6/index.js
@@ -0,0 +1,44 @@
+import { Server } from 'socket.io';
+import path from 'path';
+import http from 'http';
+import fs, { createReadStream } from 'fs';
+
+const __dirname = path.resolve();
+
+const server = http.createServer((request, response) => {
+  const indexPath = path.join(__dirname, '/lesson_6/index.html');
+
+  if (request.method === 'GET') {
+    const readStream = fs.createReadStream(indexPath);
+    readStream.pipe(response);
+  } else if (request.method === 'POST') {
+    let data = '';
+    request.on('data', chunk => {
+      data += chunk;
+    });
+    request.on('end', () => {
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+      response.writeHead(200, { 'Content-Type': 'json' });
+      response.end(data);
+    });
+  } else {
+    response.statusCode = 405;
+    response.end();
+  }
+});
+
+const socket = new Server(server);
+
+socket.on('connection', (socket) => {
+  console.log('New connection', socket.server.eio.clientsCount);
+  socket.emit('SERVER_CONNECTIONS', socket.server.eio.clientsCount);
+  socket.broadcast.emit('SERVER_CONNECTIONS', socket.server.eio.clientsCount);
+
+  socket.on('CLIENT_MSG', (data) => {
+    socket.emit('SERVER_MSG', { msg: data.msg, name: data.name });
+    socket.broadcast.emit('SERVER_MSG', { msg: data.msg, name: data.name });
+  });
+});
+
+server.listen(3000, 'localhost');
